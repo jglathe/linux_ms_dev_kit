49be5ed79e6e292fbf9c27e0fda435aab0fe398e
Elliot Berman <quic_eberman@quicinc.com>
2023-12-15 16:20:51 -0800

    gunyah: vm_mgr: Add ioctls to support basic non-proxy VM boot
    Add remaining ioctls to support non-proxy VM boot:
    
     - Gunyah Resource Manager uses the VM's devicetree to configure the
       virtual machine. The location of the devicetree in the guest's
       virtual memory can be declared via the SET_DTB_CONFIG ioctl.
     - Trigger start of the virtual machine with VM_START ioctl.
    
    Reviewed-by: Alex Elder <elder@linaro.org>
    Co-developed-by: Prakruthi Deepak Heragu <quic_pheragu@quicinc.com>
    Signed-off-by: Prakruthi Deepak Heragu <quic_pheragu@quicinc.com>
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

--- drivers/virt/gunyah/vm_mgr.h
+++ drivers/virt/gunyah/vm_mgr.h
@@ -8,6 +8,10 @@
 
 #include <linux/device.h>
 #include <linux/gunyah_rsc_mgr.h>
+#include <linux/kref.h>
+#include <linux/notifier.h>
+#include <linux/rwsem.h>
+#include <linux/wait.h>
 
 #include <uapi/linux/gunyah.h>
 
@@ -15,8 +19,15 @@ long gunyah_dev_vm_mgr_ioctl(struct gunyah_rm *rm, unsigned int cmd,
 			     unsigned long arg);
 
 struct gunyah_vm {
+	u16 vmid;
 	struct gunyah_rm *rm;
 	struct device *parent;
+	enum gunyah_rm_vm_auth_mechanism auth;
+
+	struct notifier_block nb;
+	enum gunyah_rm_vm_status vm_status;
+	wait_queue_head_t vm_status_wait;
+	struct rw_semaphore status_lock;
 };
 
 #endif
