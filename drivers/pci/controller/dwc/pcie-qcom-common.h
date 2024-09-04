10044e3d9adfe6432754c79b2bdee9d852ede067
Shashank Babu Chinta Venkata <quic_schintav@quicinc.com>
2024-09-04 12:41:59 +0530

    PCI: qcom: Add equalization settings for 16.0 GT/s
    During high data transmission rates such as 16.0 GT/s, there is an
    increased risk of signal loss due to poor channel quality and interference.
    This can impact receiver's ability to capture signals accurately. Hence,
    signal compensation is achieved through appropriate lane equalization
    settings at both transmitter and receiver. This will result in increased
    PCIe signal strength.
    
    Signed-off-by: Shashank Babu Chinta Venkata <quic_schintav@quicinc.com>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    [mani: dropped the code refactoring and minor changes]
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Link: https://lore.kernel.org/r/20240904-pci-qcom-gen4-stability-v6-3-ec39f7ae3f62@linaro.org
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>

---
+++ drivers/pci/controller/dwc/pcie-qcom-common.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#include "pcie-designware.h"
+
+void qcom_pcie_common_set_16gt_eq_settings(struct dw_pcie *pci);
