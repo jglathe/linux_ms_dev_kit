2744fbf1d8971efc8a7425f083cc65eb8c12f7c7
Jason Chen <jason.z.chen@intel.com>
2024-12-19 03:38:36 +0800

    UBUNTU: SAUCE: ACPI: scan: Update HID for new platform
    BugLink: https://bugs.launchpad.net/bugs/2090932
    
    To resolve the probe-ordering issue on the newer platform
    Update the HID for LNL and ARL
    
    Signed-off-by: Hao Yao <hao.yao@intel.com>
    Signed-off-by: Jason Chen <jason.z.chen@intel.com>
    (cherry picked from commit https://raw.githubusercontent.com/drhooray/vision-drivers/6424af8b225f136375f1a8b01500217d0545c8e6/patch/0001-ACPI-scan-Update-HID-for-new-platform.patch)
    Signed-off-by: You-Sheng Yang <vicamo.yang@canonical.com>
    Acked-by: Philip Cox <philip.cox@canonical.com>
    Acked-by: John Cabaj <john.cabaj@canonical.com>
    Signed-off-by: Timo Aaltonen <timo.aaltonen@canonical.com>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -859,9 +859,8 @@ static const char * const acpi_honor_dep_ids[] = {
 	"INTC1095", /* IVSC (ADL) driver must be loaded to allow i2c access to camera sensors */
 	"INTC100A", /* IVSC (RPL) driver must be loaded to allow i2c access to camera sensors */
 	"INTC10CF", /* IVSC (MTL) driver must be loaded to allow i2c access to camera sensors */
-	"INTC10DE", /* CVS (LNL) driver must be loaded to allow camera streaming */
-	"INTC10E0", /* CVS (ARL) driver must be loaded to allow camera streaming */
-	"INTC10E1", /* CVS (PTL) driver must be loaded to allow camera streaming */
+	"INTC10DE", /* IVSC (LNL) driver must be loaded to allow i2c access to camera sensors */
+	"INTC10E0", /* IVSC (ARL) driver must be loaded to allow i2c access to camera sensors */
 	"RSCV0001", /* RISC-V PLIC */
 	"RSCV0002", /* RISC-V APLIC */
 	"RSCV0005", /* RISC-V SBI MPXY MBOX */
