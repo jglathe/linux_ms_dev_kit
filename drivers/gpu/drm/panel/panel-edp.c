79cf1f942afe94364a48e3992a17202866f9c2c2
Jens Glathe <jens.glathe@oldschoolsolutions.biz>
2025-12-26 14:55:23 +0100

    drm/panel-edp: add panel BOE NV153WUM-N41
    found in Lenovo Ideapad Slim 3 15Q8X10
    
    raw EDID:
    
    00 ff ff ff ff ff ff 00 09 e5 ea 0c 00 00 00 00
    0c 22 01 04 a5 21 15 78 03 af e5 97 5e 58 92 28
    1f 50 54 00 00 00 01 01 01 01 01 01 01 01 01 01
    01 01 01 01 01 01 9c 3e 80 c8 70 b0 3c 40 30 20
    36 00 49 ce 10 00 00 1a 00 00 00 fd 00 28 3c 4c
    4c 10 01 0a 20 20 20 20 20 20 00 00 00 fe 00 42
    4f 45 20 43 51 0a 20 20 20 20 20 20 00 00 00 fc
    00 4e 56 31 35 33 57 55 4d 2d 4e 34 31 0a 01 63
    
    70 20 79 02 00 81 00 15 74 1a 00 00 03 01 28 3c
    00 00 53 4a 53 4a 3c 00 00 00 00 80 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 e3 90
    
    Detailed Timing Descriptors:
    DTD 1:
    1920x1200   60.002995 Hz  16:10    75.604 kHz    160.280000 MHz (329 mm x 206 mm)
       Hfront   48 Hsync  32 Hback  120 Hpol P
       Vfront    3 Vsync   6 Vback   51 Vpol N
    
    Signed-off-by: Jens Glathe <jens.glathe@oldschoolsolutions.biz>

--- drivers/gpu/drm/panel/panel-edp.c
+++ drivers/gpu/drm/panel/panel-edp.c
@@ -2032,6 +2032,7 @@ static const struct edp_panel_entry edp_panels[] = {
 	EDP_PANEL_ENTRY('B', 'O', 'E', 0x0c93, &delay_200_500_e200, "Unknown"),
 	EDP_PANEL_ENTRY('B', 'O', 'E', 0x0cb6, &delay_200_500_e200, "NT116WHM-N44"),
 	EDP_PANEL_ENTRY('B', 'O', 'E', 0x0cc9, &delay_200_500_e50, "NE120DRM-N28"),
+	EDP_PANEL_ENTRY('B', 'O', 'E', 0x0cea, &delay_200_500_e80, "NV153WUM-N41"),
 	EDP_PANEL_ENTRY('B', 'O', 'E', 0x0cf2, &delay_200_500_e200, "NV156FHM-N4S"),
 	EDP_PANEL_ENTRY('B', 'O', 'E', 0x0cf6, &delay_200_500_e200_d100, "NV140WUM-N64"),
 	EDP_PANEL_ENTRY('B', 'O', 'E', 0x0cfa, &delay_200_500_e50, "NV116WHM-A4D"),
