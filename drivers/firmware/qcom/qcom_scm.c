cdd084d9d94de2c990872a8725119a5afbc3224f
Juerg Haefliger <juerg.haefliger@canonical.com>
2026-03-17 17:27:49 +0100

    UBUNTU: SAUCE: firmware: qcom: scm: Allow QSEECOM for HP EliteBook 6 G1q
    This is required to get access to efivars and uefi boot loader support.
    
    Signed-off-by: Juerg Haefliger <juerg.haefliger@canonical.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -2296,6 +2296,7 @@ static const struct of_device_id qcom_scm_qseecom_allowlist[] __maybe_unused = {
 	{ .compatible = "dell,latitude-7455" },
 	{ .compatible = "dell,xps13-9345" },
 	{ .compatible = "ecs,liva-qc710" },
+	{ .compatible = "hp,elitebook-6-g1q" },
 	{ .compatible = "hp,elitebook-ultra-g1q" },
 	{ .compatible = "hp,omnibook-x14" },
 	{ .compatible = "huawei,gaokun3" },
