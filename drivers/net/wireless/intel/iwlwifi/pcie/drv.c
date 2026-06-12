Clome/ubuntu-kernel

UBUNTU: SAUCE: iwlwifi: add new pci id for 6235
lspci output:
Network controller [0280]: Intel Corporation Centrino Advanced-N6235
 [8086:088f] (rev 24)
 Subsystem: Intel Corporation Centrino Advanced-N 6235 [8086:526a]

BugLink: https://bugs.launchpad.net/bugs/1920180

Signed-off-by: Alex Hung <alex.hung@canonical.com>
Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

drivers/net/wireless/intel/iwlwifi/pcie/drv.c
@@ -210,6 +210,7 @@ static const struct pci_device_id iwl_hw_card_ids[] = {
 	{IWL_PCI_DEVICE(0x088E, 0x446A, iwl6035_2agn_sff_cfg)},
 	{IWL_PCI_DEVICE(0x088E, 0x4860, iwl6035_2agn_cfg)},
 	{IWL_PCI_DEVICE(0x088F, 0x5260, iwl6035_2agn_cfg)},
+	{IWL_PCI_DEVICE(0x088F, 0x526A, iwl6035_2agn_cfg)},
 
 /* 105 Series */
 	{IWL_PCI_DEVICE(0x0894, 0x0022, iwl105_bgn_cfg)},

jglathe/linux_ms_dev_kit

drivers/net/wireless/intel/iwlwifi/pcie/drv.c
@@ -208,6 +208,7 @@ VISIBLE_IF_IWLWIFI_KUNIT const struct pci_device_id iwl_hw_card_ids[] = {
 	{IWL_PCI_DEVICE(0x088E, 0x446A, iwl6030_mac_cfg)},
 	{IWL_PCI_DEVICE(0x088E, 0x4860, iwl6030_mac_cfg)},
 	{IWL_PCI_DEVICE(0x088F, 0x5260, iwl6030_mac_cfg)},
+	{IWL_PCI_DEVICE(0x088F, 0x526A, iwl6030_mac_cfg)},
 
 /* 105 Series */
 	{IWL_PCI_DEVICE(0x0894, 0x0022, iwl105_mac_cfg)},