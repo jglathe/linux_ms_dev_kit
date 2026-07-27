20c2ea692ef03ac73a88bf85c20efe6643ed8659
Jens Glathe <jens.glathe@oldschoolsolutions.biz>
2026-07-27 21:21:19 +0200

    phy: qcom: qmp-combo: elevate explicit mode message to dev_info
    need to make sure the setting gets respected
    
    Signed-off-by: Jens Glathe <jens.glathe@oldschoolsolutions.biz>

--- drivers/phy/qualcomm/phy-qcom-qmp-combo.c
+++ drivers/phy/qualcomm/phy-qcom-qmp-combo.c
@@ -4933,7 +4933,7 @@ static int qmp_combo_probe(struct platform_device *pdev)
 		if (ret)
 			goto err_node_put;
 	} else if (explicit_mode) {
-		dev_dbg(dev, "Static explicit mode %s enabled\n", qmpphy_mode_str[qmp->qmpphy_mode]);
+		dev_info(dev, "Static explicit mode %s enabled\n", qmpphy_mode_str[qmp->qmpphy_mode]);
 	} else {
 		enum typec_orientation dp_orientation = TYPEC_ORIENTATION_NONE;
 		enum typec_orientation usb3_orientation = TYPEC_ORIENTATION_NONE;
